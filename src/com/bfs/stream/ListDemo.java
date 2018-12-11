176f98df9face29a63172481dc924d92c60dc480
bfansheng <bfansheng@gmail.com>
2018-12-11 16:18:50 +0800

    stream

--- src/com/bfs/stream/ListDemo.java
+++ src/com/bfs/stream/ListDemo.java
@@ -1,28 +1,25 @@
-package com.bfs.lamda.list;
+package com.bfs.stream;
 
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * @author lwb
- * @date 2018-12-10
+ * @date 2018/12/11
  */
 public class ListDemo {
 
-
+    private List<Integer> list = Arrays.asList(1, 3, 5, 7, 9);
 
     @Test
-    public void sort() {
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
-        list.sort(Comparator.comparingInt(o -> o));
-        println(list);
-
-        list.sort((a, b) -> b -a);
-        println(list);
+    public void test() {
+        Objects.requireNonNull(list);
+        List<Integer> collect = list.stream().sorted((a, b) -> b -a).collect(Collectors.toList());
+        println(collect);
     }
 
     private <T>void println(List<T> list) {
@@ -31,4 +28,5 @@ public class ListDemo {
         list.forEach(System.out::println);
         System.out.println("================end===================\n");
     }
+
 }
